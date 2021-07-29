36b180a39d11f0b70e9c64238607b2877f0b1b50
baejunil <70627982+baejunil@users.noreply.github.com>
2021-07-30 01:06:12 +0900

    gitignore google-services.json

--- android/app/src/main/java/com/example/stacklounge/MainActivity.kt
+++ android/app/src/main/java/com/example/stacklounge/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.stacklounge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stacklounge.databinding.ActivityMainBinding
+import com.example.stacklounge.login.LoginActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        if (Firebase.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+            finish()
+        }
+
     }
 }
