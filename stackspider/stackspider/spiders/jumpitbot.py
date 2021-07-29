1fbc4c7784128c3773be727002cbf972201b1c00
Sean Hong(홍성민) <seanhong2000@gmail.com>
2021-07-30 05:22:18 +0900

    Refactor python scripts in black linter format

--- stackspider/stackspider/spiders/jumpitbot.py
+++ stackspider/stackspider/spiders/jumpitbot.py
@@ -5,30 +5,32 @@ from urllib.parse import urljoin, urlparse
 import requests
 import json
 
+
 class JumpitbotSpider(Spider):
-    name = 'jumpitbot'
-    allowed_domains = ['jumpit.co.kr']
-    start_urls = ['https://www.jumpit.co.kr']
+    name = "jumpitbot"
+    allowed_domains = ["jumpit.co.kr"]
+    start_urls = ["https://www.jumpit.co.kr"]
     custom_settings = {
-        'ITEM_PIPELINES': {
-            'stackspider.pipelines.JumpitPipeline' : 300,
+        "ITEM_PIPELINES": {
+            "stackspider.pipelines.JumpitPipeline": 300,
         }
     }
+
     def parse(self, response):
-        '''
-        웹에서 list?page api를 토대로 전체 page의 개수를 찾고 
+        """
+        웹에서 list?page api를 토대로 전체 page의 개수를 찾고
         page url을 호출한다.
             yield:
                 각 page를 호출하여 callback 함수에서 반환된 값.
-                반환 코드는 
+                반환 코드는
                         정상 호출 시 : <200>
                         redirect 시 : <3**>
                         도중 문제 발생 시 : <4**> 또는 <5**>
-        '''
-        
+        """
+
         page_num = 0
         while True:
-            api_data = urljoin(response.url, f'/api/position/list?page={page_num}')
+            api_data = urljoin(response.url, f"/api/position/list?page={page_num}")
             data = requests.get(api_data).text
             if len(data) < 400:
                 break
@@ -36,46 +38,40 @@ class JumpitbotSpider(Spider):
             yield Request(api_data, callback=self.parseCompanyApiContent)
 
     def parseCompanyApiContent(self, response):
-        '''
+        """
         기업 공고문의 각 페이지에 대한url을 얻는다.
         yield:
                 company id 만 따로 정리해준 url을 호출한다.
                 Request:
                     url : company id별 정보 페이지 url
                     callback : 호출 및 값을 반환할 callback 함수
-                반환 코드는 
+                반환 코드는
                         정상 호출 시 : <200>
                         redirect 시 : <3**>
                         도중 문제 발생 시 : <4**> 또는 <5**>
-        '''
+        """
         response_body = json.loads(response.text)
 
-        for i in range(len(response_body['result']['content'])):
-            company_id = response_body['result']['content'][i]['id']
-            url = urljoin(response.url[:29], 'position/'+str(company_id))
+        for i in range(len(response_body["result"]["content"])):
+            company_id = response_body["result"]["content"][i]["id"]
+            url = urljoin(response.url[:29], "position/" + str(company_id))
             yield Request(url, callback=self.parseJobContent)
 
-    
     def parseJobContent(self, response):
-        '''
-            Company ID에 따른 api url에서 가져온 json값을 정리한다.
-        '''
+        """
+        Company ID에 따른 api url에서 가져온 json값을 정리한다.
+        """
         json_ = json.loads(response.text)
-        json_response = json_['result']
+        json_response = json_["result"]
 
         tech = []
-        for i in range(len(json_response['techStacks'])):
-            tech.append(json_response['techStacks'][i]['stack'])
+        for i in range(len(json_response["techStacks"])):
+            tech.append(json_response["techStacks"][i]["stack"])
 
         tech_stacks = {
-            "id" : int(json_response['companyProfileId']),
-            "name" : json_response['companyName'],
-            "tech_stack" : tech
+            "id": int(json_response["companyProfileId"]),
+            "name": json_response["companyName"],
+            "tech_stack": tech,
         }
 
         yield tech_stacks
-        
-
-
-       
-        
--- stackspider/stackspider/spiders/kiwizzlebot.py
+++ stackspider/stackspider/spiders/kiwizzlebot.py
@@ -3,21 +3,28 @@ from collections import defaultdict
 from stackspider.items import CompanyItem
 import json
 
+# "https://static.toss.im/greenhouse/jobs/jobs.json?v=2"
+# "https://career.woowahan.com/w1/recruits?category=jobGroupCodes%3ABA005001&recruitCampaignSeq=0&jobGroupCodes=BA005001&page=0&size=100&sort=updateDate%2Cdesc"
+
+
 class KiwizzlebotSpider(Spider):
-    name = 'kiwizzlebot'
-    allowed_domains = ['kiwizzle.com/search']
-    start_urls = [f"https://kiwizzle.com/api/v1/job/{i}" for i in range(1,4800)]
-    
+    """
+    키위즐(https://programmers.co.kr)
+    개발자 채용(https://programmers.co.kr/job)을 크롤하는 스파이더
+    """
+
+    name = "kiwizzlebot"
+    allowed_domains = ["kiwizzle.com/search"]
+    start_urls = [f"https://kiwizzle.com/api/v1/job/{i}" for i in range(1, 4800)]
+
     custom_settings = {
-        'ITEM_PIPELINES': {
-            'stackspider.pipelines.KiwizzlePipeline' : 300,
+        "ITEM_PIPELINES": {
+            # "stackspider.pipelines.KiwizzlePipeline": 300,  # 호스팅 하면 해당 파이프라인 다시 추가하기
+            "stackspider.pipelines.RawPipeline": 800,
         }
     }
-    def parse(self, response):    
-        if(response.status == 200):            
+
+    def parse(self, response):
+        if response.status == 200:
             jsons = json.loads(response.text)
             yield jsons
-        
-        
-             
-    
