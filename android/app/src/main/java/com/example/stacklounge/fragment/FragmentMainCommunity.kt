820b117c9d528f3a40a056dd344fe846ea7981d5
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2021-08-28 02:53:36 +0900

    게시판 틀 완성

--- android/app/src/main/java/com/example/stacklounge/fragment/FragmentMainCommunity.kt
+++ android/app/src/main/java/com/example/stacklounge/fragment/FragmentMainCommunity.kt
@@ -3,11 +3,12 @@ package com.example.stacklounge.fragment
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.stacklounge.board.AdapterCommunityBoard
 import com.example.stacklounge.R
-import com.google.android.material.snackbar.Snackbar
+import com.example.stacklounge.board.BoardData
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_main_community.view.*
 
@@ -67,6 +68,24 @@ class FragmentMainCommunity : Fragment() {
         view.btnBoardUpdate.setOnClickListener {
 
         }
+
+        // 데이터를 db에서 가져와야한다.
+        var boardList = arrayListOf<BoardData>(
+            BoardData("firsttitle","firstcontents","12:01"),
+            BoardData("second","secondcontents","12:02"),
+            BoardData("third","thirdcontents","12:03"),
+            BoardData("fourth","fourthcontents","12:04")
+
+        )
+
+        //board recycleview를 community에 부착
+        val rAdapter = AdapterCommunityBoard(context,boardList)
+        rAdapter.notifyDataSetChanged() //어댑터의 데이터가 변했다는 notify를 날린다
+        view.boardRecycleview.adapter = rAdapter
+
+        val lm = LinearLayoutManager(context)
+        view.boardRecycleview.layoutManager = lm
+        view.boardRecycleview.setHasFixedSize(true)
 
         return view
     }
