c755d6f2e025baf9b6bc536a68f2ad5ca341390f
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2021-09-07 04:36:21 +0900

    FragmentMainUser에서 github ID 적용

--- android/app/src/main/java/com/example/stacklounge/fragment/FragmentMainUser.kt
+++ android/app/src/main/java/com/example/stacklounge/fragment/FragmentMainUser.kt
@@ -3,21 +3,22 @@ package com.example.stacklounge.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.stacklounge.R
 import com.example.stacklounge.login.LoginActivity
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_board_write_text.*
 import kotlinx.android.synthetic.main.fragment_main_user.*
 import kotlinx.android.synthetic.main.fragment_main_user.view.*
 
 
 class FragmentMainUser : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +29,9 @@ class FragmentMainUser : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_main_user, null)
 
+        // github ID 불러오기
+        getUserId()
+
         //로그아웃 버튼
         view.btnlogout.setOnClickListener {
             Firebase.auth.signOut()
@@ -37,8 +41,32 @@ class FragmentMainUser : Fragment() {
 
         }
 
+
+
         return view
 
+    }
+
+    fun getUserId(){
+        // db
+        val database = FirebaseDatabase.getInstance("https://stacklounge-62ffd-default-rtdb.asia-southeast1.firebasedatabase.app/") // 프로젝트 주소
+        val userIdRef = database.getReference("current-user") // userId 불러오는 경로
+
+        userIdRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val aUserId = snapshot.child("login").value
+
+                tvUserIDinUser.text = aUserId.toString()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //실패할 때
+                Toast.makeText(activity,"DB 에러", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
 }
